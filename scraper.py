c9e8950b1ad811a76dd4acda03f874c4149db354
cuteparrot <clear.rin04@gmail.com>
2025-02-02 23:57:45 +0900

    에타 스크래핑 로그인 로직 설정

--- scraper.py
+++ scraper.py
@@ -1,7 +1,25 @@
 import selenium
 
-def main():
+def open_browser():
     pass
+
+def go_to_login_page():
+    pass
+
+def insert_account_info():
+    pass
+
+def click_login():
+    pass
+
+def login_everytime(id: str, password: str):
+    go_to_login_page()
+    insert_account_info()
+    click_login()
+
+def main():
+    open_browser()
+    login_everytime()
 
 if __name__ == "__main__":
     main()
